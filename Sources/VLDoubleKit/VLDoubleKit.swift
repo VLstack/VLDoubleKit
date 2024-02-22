d9a60df3311048b6dd554dbd88eacfce6f1b03a2
VLstack <github@vlstack.fr>
2024-02-22 13:52:22 +0100

    initial commit

--- Sources/VLDoubleKit/VLDoubleKit.swift
+++ Sources/VLDoubleKit/VLDoubleKit.swift
@@ -1,2 +1,45 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public
+extension Double
+{
+ func kmFormatted(decimals: Int = 2,
+                  handleInteger: Bool = true) -> String
+ {
+  var nb: Int = decimals
+  
+  if handleInteger && self.truncatingRemainder(dividingBy: 1) == 0
+  {
+   nb = 0
+  }
+  
+  return String(format: "%.\(nb)f km", self)
+ }
+ 
+ func percentFormatted(decimals: Int = 2,
+                       handleInteger: Bool = true) -> String
+ {
+  var nb: Int = decimals
+  
+  if handleInteger && self.truncatingRemainder(dividingBy: 1) == 0
+  {
+   nb = 0
+  }
+  
+  return String(format: "%.\(nb)f %", self)
+ }
+ 
+ func kwFormatted(decimals: Int = 2,
+                  handleInteger: Bool = true) -> String
+ {
+  var nb: Int = decimals
+  
+  if handleInteger && self.truncatingRemainder(dividingBy: 1) == 0
+  {
+   nb = 0
+  }
+    
+  return String(format: "%.\(nb)f kW", self)
+ }
+
+}
