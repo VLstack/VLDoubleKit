0a8c0a0179df6372f9112f3e9caf23c5df48e34b
VLstack <github@vlstack.fr>
2024-02-26 08:29:42 +0100

    deprecate duplicated logic to a single method

--- Sources/VLDoubleKit/VLDoubleKit.swift
+++ Sources/VLDoubleKit/VLDoubleKit.swift
@@ -3,6 +3,26 @@ import Foundation
 public
 extension Double
 {
+ func formatted(decimals: Int = 2,
+                handleInteger: Bool = true,
+                suffix: String? = nil) -> String
+ {
+  var nb: Int = decimals
+  
+  if handleInteger && self.truncatingRemainder(dividingBy: 1) == 0
+  {
+   nb = 0
+  }
+  
+  if let suffix
+  {
+   return String(format: "%.\(nb)f \(suffix)", self)
+  }
+  
+  return String(format: "%.\(nb)f", self)
+ }
+
+ @available(*, deprecated, renamed: "formatted(decimals:handleInteger:suffix:)")
  func kmFormatted(decimals: Int = 2,
                   handleInteger: Bool = true) -> String
  {
@@ -16,6 +36,7 @@ extension Double
   return String(format: "%.\(nb)f km", self)
  }
  
+ @available(*, deprecated, renamed: "formatted(decimals:handleInteger:suffix:)")
  func percentFormatted(decimals: Int = 2,
                        handleInteger: Bool = true) -> String
  {
@@ -29,6 +50,7 @@ extension Double
   return String(format: "%.\(nb)f %", self)
  }
  
+ @available(*, deprecated, renamed: "formatted(decimals:handleInteger:suffix:)")
  func kwFormatted(decimals: Int = 2,
                   handleInteger: Bool = true) -> String
  {
