9e96761e929da7e9be4162446ed2ccbc659f05a1
VLstack <github@vlstack.fr>
2024-05-04 09:18:47 +0200

    deprecate parameter "forceInteger", replaced by "decimals: 0"

--- Sources/VLDoubleKit/VLDoubleKit.swift
+++ Sources/VLDoubleKit/VLDoubleKit.swift
@@ -3,23 +3,29 @@ import Foundation
 public extension Double
 {
  func formatted(decimals: Int = 2,
-                forceInteger: Bool = true,
                 suffix: String? = nil,
                 separator: String? = nil) -> String
  {
-  var value: String
-  if forceInteger && self.truncatingRemainder(dividingBy: 1) == 0
-  {
-   value = String(format: "%.0f", self)
-  }
-  else
-  {
-   let decimalSeparator: String = separator ?? Locale.current.decimalSeparator ?? "."
-   value = String(format: "%.\(decimals)f", self).replacingOccurrences(of: ".", with: decimalSeparator)
-  }
+  let decimalSeparator: String = separator ?? Locale.current.decimalSeparator ?? "."
+  let nbDecimals: Int = max(0, decimals)
+  let value: String = String(format: "%.\(nbDecimals)f", self).replacingOccurrences(of: ".", with: decimalSeparator)
 
   if let suffix { return "\(value) \(suffix)" }
 
   return value
+ }
+
+ @available(*, deprecated, message: "forceInteger parameter must be replaced by decimals: 0")
+ func formatted(decimals: Int = 2,
+                forceInteger: Bool,
+                suffix: String? = nil,
+                separator: String? = nil) -> String
+ {
+  if forceInteger
+  {
+   return self.formatted(decimals: 0, suffix: suffix, separator: separator)
+  }
+
+  return self.formatted(decimals: decimals, suffix: suffix, separator: separator)
  }
 }
