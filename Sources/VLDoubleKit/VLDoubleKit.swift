dfa588f4da97d51dcb5bffadfaf450b21f7c0853
VLstack <github@vlstack.fr>
2024-05-04 09:31:31 +0200

    reintroduce "allowInteger" parameter since it was an error to deprecate it

--- Sources/VLDoubleKit/VLDoubleKit.swift
+++ Sources/VLDoubleKit/VLDoubleKit.swift
@@ -3,29 +3,24 @@ import Foundation
 public extension Double
 {
  func formatted(decimals: Int = 2,
+                allowInteger: Bool = false,
                 suffix: String? = nil,
                 separator: String? = nil) -> String
  {
   let decimalSeparator: String = separator ?? Locale.current.decimalSeparator ?? "."
-  let nbDecimals: Int = max(0, decimals)
+  let nbDecimals: Int
+  if allowInteger && self.truncatingRemainder(dividingBy: 1) == 0
+  {
+   nbDecimals = 0
+  }
+  else
+  {
+   nbDecimals = max(0, decimals)
+  }
   let value: String = String(format: "%.\(nbDecimals)f", self).replacingOccurrences(of: ".", with: decimalSeparator)
 
   if let suffix { return "\(value) \(suffix)" }
 
   return value
- }
-
- @available(*, deprecated, message: "forceInteger parameter must be replaced by decimals: 0")
- func formatted(decimals: Int = 2,
-                forceInteger: Bool,
-                suffix: String? = nil,
-                separator: String? = nil) -> String
- {
-  if forceInteger
-  {
-   return self.formatted(decimals: 0, suffix: suffix, separator: separator)
-  }
-
-  return self.formatted(decimals: decimals, suffix: suffix, separator: separator)
  }
 }
